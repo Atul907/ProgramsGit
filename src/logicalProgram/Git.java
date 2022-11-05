1643629dc876f410149311da05ece3d127a8eece
Atul907 <atulmishra907@gmail.com>
2022-11-05 16:19:09 +0530

    git practise

---
+++ src/logicalProgram/Git.java
@@ -0,0 +1,11 @@
+package logicalProgram;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is git from eclipse");
+	}
+
+}
